193b2ed6f95f2379974d5ea6054cde2742fabcf7
Mamadou AMADOU <doumstyle@gmail.com>
2021-12-16 16:22:23 +0100

    Updated URLs of some assets once more! Added event listener on audio element

--- js/start.js
+++ js/start.js
@@ -1,10 +1,12 @@
-const musicUrl = "./../assets/music/title-screen-music.mp3";
+const musicUrl = "Ironhack-project-1/assets/music/title-screen-music.mp3";
 const music = new Audio(musicUrl);
-const selection = "./../assets/sounds/selection.wav";
+const selection = "Ironhack-project-1/assets/sounds/selection.wav";
 const gameStartSound = new Audio(selection);
 
 window.onmouseover = () => {
-    music.play();
+    music.addEventListener('canplaythrough', () => {
+        music.load();
+    });
     const title = document.querySelector('#title h1');
     title.classList.add('rotate');
 }
